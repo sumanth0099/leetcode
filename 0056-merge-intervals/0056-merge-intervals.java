324b0972d6296bf1b5f49e43ac01b82cc8f62f9e
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2025-01-31 17:18:05 +0530

    Time: 9 ms (48.97%), Space: 47.1 MB (10.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
         List<int[]> l = new ArrayList<>();
         int a = intervals[0][0];
         int b = intervals[0][1];
-
         for (int i = 1; i < intervals.length; i++) {
             if (b >= intervals[i][0]) {
-                b = Math.max(b, intervals[i][1]); // Merge intervals properly
+                b = Math.max(b, intervals[i][1]);
             } else {
                 l.add(new int[]{a, b});
                 a = intervals[i][0];
@@ -16,9 +14,7 @@ class Solution {
             }
         }
 
-        l.add(new int[]{a, b}); // Add the last interval
-
-        // Convert List<int[]> to int[][]
+        l.add(new int[]{a, b});
         return l.toArray(new int[l.size()][]);
     }
 }
