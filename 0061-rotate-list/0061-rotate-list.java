1113e41a0fd7ac24cbd2ff6738269acf255ea414
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2025-03-07 23:16:06 +0530

    Time: 0 ms (100%), Space: 42.5 MB (78.58%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -3,28 +3,21 @@ class Solution {
         if (head == null || head.next == null || k == 0)
             return head;
 
-        // Find length of the list
         int length = 1;
         ListNode t = head;
         while (t.next != null) {
             length++;
             t = t.next;
         }
-
-        // Reduce k within bounds
         k = k % length;
-        if (k == 0) return head;  // No rotation needed
+        if (k == 0) return head;  
 
-        // Make it circular
         t.next = head;
         
-        // Find new tail (length - k - 1) steps from head
         t = head;
         for (int i = 0; i < length - k - 1; i++) {
             t = t.next;
         }
-
-        // Set new head and break the cycle
         ListNode newHead = t.next;
         t.next = null;
         
