36715df3a6b7df9cbbeee9d61fe8312ea4b46221
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2026-05-11 07:14:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,7 +1,14 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
-        for(int each:nums)
-        System.out.print(each+" ");
+        int freq[] = new int[3];
+        for(int i:nums)
+        freq[i]++;
+        int i = 0;
+        while(i<freq[0])
+        nums[i++]=0;
+            while(i<freq[1]+freq[0])
+        nums[i++]=1;
+                while(i<nums.length)
+        nums[i++]=2;
     }
 }
