5e23156bcb99f31b66adb1574c77830c2a2d26ad
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2026-05-11 09:09:47 +0530

    Time: 3 ms (55.5%), Space: 47 MB (72.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,34 +1,19 @@
+import java.util.*;
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int p[] = new int[2];
-     for(int i=0;i<nums.length;i++)
-     {
-        int left = i+1;
-        int right = nums.length-1;
-        while(left<=right)
-        {
-            if((nums[i]+nums[left])==target)
-            {
-                p[0] = i;
-                p[1]=left;
-                return p;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
-           else if((nums[i]+nums[right])==target)
-            {
-                p[0] = i;
-                p[1]=right;
-                return p;
-            }
-            else if(left!=right && (nums[left]+nums[right])==target)
-            {
-                p[0] = left;
-                p[1]=right;
-                return p;
-            }
-            left++;
-            right--;
+
+            map.put(nums[i], i);
         }
-     }   
-     return p;
+
+        return new int[]{-1, -1};
     }
 }
