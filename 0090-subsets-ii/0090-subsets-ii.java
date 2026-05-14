a5f53c8c52e7b03e841e3097beb2c57efecab28f
Tetala Sumanth Reddy <142477844+sumanth0099@users.noreply.github.com>
2026-05-14 11:46:38 +0530

    Time: 4 ms (16.12%), Space: 44.7 MB (97.04%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,19 +1,21 @@
+import java.util.*;
 class Solution {
-    Set<List<Integer>>fullset = new HashSet<>();
-    List<Integer>l = new ArrayList<>();
-    void helper(int arr[],int start){
-        fullset.add(new ArrayList<>(l));
-        for(int i = start;i<arr.length;i++)
+    public static HashMap<ArrayList<Integer>,Boolean>table = new HashMap<>();
+    public static void PrintSubSub(int nums[],ArrayList<Integer>list,int start){
+        table.put(new ArrayList<>(list),true);
+        for(int i=start;i<nums.length;i++)
         {
-            l.add(arr[i]);
-            helper(arr,i+1);
-            l.remove(l.size()-1);
+
+            list.add(nums[i]);
+            PrintSubSub(nums, list, i+1);
+            list.remove(list.size()-1);
         }
-    }   
+    }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        table.clear();
         Arrays.sort(nums);
-        helper(nums,0);
-         List<List<Integer>>fulllist = new ArrayList<>(fullset);
-         return fulllist;
+        ArrayList<Integer>l = new ArrayList<>();
+        PrintSubSub(nums, l, 0);
+        return new ArrayList<>(table.keySet());
     }
 }
